4e9e0c37404a41e388c18799e4f2f7971915f117
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2019-08-05 00:42:33 +0900

    expressの処理を追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,16 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+const app = express();
+const cors = require('cors');
+
+app.use(cors({ origin: true }));
+
+app.post('/webhook', (req, res) => {
+  console.log(req.headers)
+  res.json(req.body)
 });
+
+export const asana = functions.https.onRequest(app);
+
+export default app
