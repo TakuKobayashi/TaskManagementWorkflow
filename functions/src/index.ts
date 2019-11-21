0abe97712e9ff6c41b8ea006b60e3ab4d537d8ad
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-11-21 19:07:19 +0900

    asana のrooterを設定

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,25 +1,16 @@
 import {region} from "firebase-functions";
 import * as express from 'express';
+import { router } from './api/routes/asana';
 
 const app = express();
 const cors = require('cors');
 
 app.use(cors({ origin: true }));
 
+app.use('/asana', router);
+
 app.get('/', (req, res) => {
   res.json({hello: "world"})
-});
-
-app.post('/webhook', (req, res) => {
-  const hookSecret = req.headers["x-hook-secret"]
-  console.log(hookSecret)
-  if(hookSecret){
-    res.setHeader('X-Hook-Secret', hookSecret);
-  }
-  console.log(req.headers)
-  console.log(req.query)
-  console.log(req.body)
-  res.json(req.body)
 });
 
 export const api = region("asia-northeast1").https.onRequest(app);
