80a094c9a27c05aab3236f8067b0390eb632388b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-11-22 12:57:21 +0900

    load firestore

---
+++ functions/src/libs/load-firestore.ts
@@ -0,0 +1,8 @@
+import * as admin from "firebase-admin";
+import * as functions from 'firebase-functions';
+
+export function initFirestore(): admin.firestore.Firestore{
+    admin.initializeApp(functions.config().firebase);
+    return admin.firestore();
+}
+
