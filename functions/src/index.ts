d3f2186a7b47ac3eb1c0320b86082bc459ccc054
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-11-22 13:17:17 +0900

    firestoreについてのサンプルを入れてみた

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,10 @@ import { asanaRouter } from './api/routes/asana';
 import { githubRouter } from './api/routes/github';
 import { googleRouter } from './api/routes/google';
 
+import { initFirestore } from './libs/load-firestore';
+
+const firestore = initFirestore();
+
 const app = express();
 const cors = require('cors');
 
@@ -15,6 +19,27 @@ app.use('/github', githubRouter);
 
 app.get('/', (req, res) => {
   res.json({ hello: 'world' });
+});
+
+app.get('/getData', async (req, res) => {
+  const users = await firestore.collection('users').get()
+  const results: FirebaseFirestore.DocumentData[] = [];
+  users.forEach((doc: FirebaseFirestore.DocumentData) => {
+    console.log(doc.id, '=>', doc.data());
+    results.push(doc.data())
+  });
+  res.json(results);
+});
+
+app.get('/inputData', (req, res) => {
+  // collectionがデータのリスト(row) docがキー(Id)で中身がカラム(JSON)
+  const docRef = firestore.collection('users').doc('alovelace');
+  const setAda = docRef.set({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815,
+  });
+  res.json(setAda);
 });
 
 export const api = region('asia-northeast1').https.onRequest(app);
