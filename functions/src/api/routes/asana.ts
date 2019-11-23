be71e8a11132458dafa744749fceebf7d6c0a1f9
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2019-11-23 20:23:25 +0900

    asanaのデータを取得するAPI作成

--- functions/src/api/routes/asana.ts
+++ functions/src/api/routes/asana.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-//import * as Asana from 'asana';
+import * as Asana from 'asana';
 
 const express = require('express');
 const passport = require('passport');
@@ -9,16 +9,22 @@ asanaRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('hello asana');
 });
 
+asanaRouter.get('/workspaces', async (req: Request, res: Response, next: NextFunction) => {
+  const asanaAccessToken = req.get('asana-access-token') || '';
+  const client = Asana.Client.create().useAccessToken(asanaAccessToken);
+  const meUser = await client.users.me()
+  res.json(meUser.workspaces);
+});
+
+asanaRouter.get('/projects', async (req: Request, res: Response, next: NextFunction) => {
+  const asanaAccessToken = req.get('asana-access-token') || '';
+  const workspaceId: string = req.query('workspaceId') || '';
+  const client = Asana.Client.create().useAccessToken(asanaAccessToken);
+  const projects = await client.projects.findByWorkspace(Number(workspaceId))
+  res.json(projects.data);
+});
+
 asanaRouter.get('/auth', passport.authenticate('Asana'));
-//asanaRouter.get('/auth', (req: Request, res: Response, next: NextFunction) => {
-/*
-  const client = Asana.Client.create({
-    clientId: process.env.ASANA_CLIENT_ID,
-    clientSecret: process.env.ASANA_CLIENT_SECRET,
-    redirectUri: '/ag2w-245905/asia-northeast1/api/asana/callback',
-  });
-  */
-//});
 
 asanaRouter.get('/callback', passport.authenticate('Asana'), (req: Request, res: Response, next: NextFunction) => {
   res.redirect('/');
