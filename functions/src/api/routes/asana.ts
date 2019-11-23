0e73cfd28e94edf8c82d39faa95c08f6593f7340
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2019-11-23 22:13:33 +0900

    Webhook関係の処理やAPIを作成

--- functions/src/api/routes/asana.ts
+++ functions/src/api/routes/asana.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
-import * as Asana from 'asana';
+import { AsanaWebhookRoutine } from '../../libs/asana-webhook-routine';
+const Asana = require('asana');
 
 const express = require('express');
 const passport = require('passport');
@@ -12,7 +13,7 @@ asanaRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
 asanaRouter.get('/workspaces', async (req: Request, res: Response, next: NextFunction) => {
   const asanaAccessToken = req.get('asana-access-token') || '';
   const client = Asana.Client.create().useAccessToken(asanaAccessToken);
-  const meUser = await client.users.me()
+  const meUser = await client.users.me();
   res.json(meUser.workspaces);
 });
 
@@ -20,8 +21,33 @@ asanaRouter.get('/projects', async (req: Request, res: Response, next: NextFunct
   const asanaAccessToken = req.get('asana-access-token') || '';
   const workspaceId: string = req.query('workspaceId') || '';
   const client = Asana.Client.create().useAccessToken(asanaAccessToken);
-  const projects = await client.projects.findByWorkspace(Number(workspaceId))
+  const projects = await client.projects.findByWorkspace(Number(workspaceId));
   res.json(projects.data);
+});
+
+asanaRouter.get('/registingWebhooks', async (req: Request, res: Response, next: NextFunction) => {
+  const asanaAccessToken = req.get('asana-access-token') || '';
+  const workspaceId: string = req.query('workspaceId') || '';
+  const client = Asana.Client.create().useAccessToken(asanaAccessToken);
+  const webhooks = await client.webhooks.getAll(workspaceId);
+  res.json(webhooks.data);
+});
+
+asanaRouter.post('/registWebhook', async (req: Request, res: Response, next: NextFunction) => {
+  const asanaAccessToken = req.get('asana-access-token') || '';
+  const projectId: string = req.query('projectId') || '';
+  const client = Asana.Client.create().useAccessToken(asanaAccessToken);
+  // 一番最後に {opt_fields: []} コレを追加するとWebhookの時に付け加えるパラメータを増やすことができる https://developers.asana.com/docs/#establish-a-webhook
+  const webhook = await client.webhooks.create(projectId, 'https://8301607d.ngrok.io/ag2w-245905/asia-northeast1/api/asana/webhook');
+  res.json(webhook);
+});
+
+asanaRouter.post('/deleteWebhook', async (req: Request, res: Response, next: NextFunction) => {
+  const asanaAccessToken = req.get('asana-access-token') || '';
+  const webhookId: string = req.query('webhookId') || '';
+  const client = Asana.Client.create().useAccessToken(asanaAccessToken);
+  await client.webhooks.deleteById(webhookId);
+  res.status(200);
 });
 
 asanaRouter.get('/auth', passport.authenticate('Asana'));
@@ -30,15 +56,17 @@ asanaRouter.get('/callback', passport.authenticate('Asana'), (req: Request, res:
   res.redirect('/');
 });
 
-asanaRouter.post('/webhook', (req: Request, res: Response, next: NextFunction) => {
+asanaRouter.post('/webhook', async (req: Request, res: Response, next: NextFunction) => {
   const hookSecret = req.headers['x-hook-secret'];
-  console.log(hookSecret);
   if (hookSecret) {
     res.setHeader('X-Hook-Secret', hookSecret);
+  } else {
+    const webhookRoutine = new AsanaWebhookRoutine(req.body);
+    await webhookRoutine.execute();
   }
-  console.log(req.headers);
-  console.log(req.query);
-  console.log(req.body);
+  console.log(JSON.stringify(req.headers));
+  console.log(JSON.stringify(req.query));
+  console.log(JSON.stringify(req.body));
   res.json(req.body);
 });
 
