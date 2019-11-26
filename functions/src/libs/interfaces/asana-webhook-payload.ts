d88068115ffbf939d1aef600fdcfc81b60fdc3d1
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-11-26 19:33:36 +0900

    AsanaWebhookPayloadの中身を定義

--- functions/src/libs/interfaces/asana-webhook-payload.ts
+++ functions/src/libs/interfaces/asana-webhook-payload.ts
@@ -1,3 +1,17 @@
 export interface AsanaWebhookPayload {
-  events: any[];
+  events: AsanaEvent[];
+}
+
+interface AsanaEvent {
+  user: AsanaResource;
+  created_at: string;
+  action: string;
+  resource: AsanaResource;
+  parent: AsanaResource;
+}
+
+interface AsanaResource {
+  gid: string;
+  resource_type: string;
+  resource_subtype: string | undefined;
 }
